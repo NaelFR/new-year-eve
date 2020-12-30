d22028d80ceb03e8e2a2314d739a753a96ed5256
Nael Abou Chahine <abou.chahine.nael@gmail.com>
2020-12-30 16:32:58 +0100

    google auth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  important: true,
+  // Active dark mode on class basis
+  darkMode: 'class',
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  purge: {
+    content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+    // These options are passed through directly to PurgeCSS
+  },
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        // check: "url('/icons/check.svg')",
+        // landscape: "url('/images/landscape/2.jpg')",
+      }),
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
+      inset: ['checked'],
+      zIndex: ['hover', 'active'],
+      opacity: ['disabled'],
+    },
   },
   plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
 };
